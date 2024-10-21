8dfcbbcd1cf2db177e801318c0ffbe47cc835669
nakshatra-tomar <tomarnakshatra@gmail.com>
2024-10-22 01:45:25 +0530

    completed inviteUserForm, invitation page

--- frontend/src/pages/Project/InviteUserForm.jsx
+++ frontend/src/pages/Project/InviteUserForm.jsx
@@ -10,16 +10,27 @@ import {
 import { useForm } from "react-hook-form";
 import React from "react";
 import { DialogClose } from "@/components/ui/dialog";
+import { inviteToProject } from "@/redux/Project/Project.Action";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const InviteUserForm = ({ projectId }) => {
+  const dispatch = useDispatch();
+
+  const { id } = useParams;
+  const onSubmit = (data) => {
+    data.projectId = projectId;
+    dispatch(inviteToProject({ email: data.email, projectId: id }));
+
+    console.log("Invitation Sent", data);
+  };
+
   const form = useForm({
     defaultValues: {
       email: "",
     },
   });
-  const onSubmit = (data) => {
-    console.log("Invitation sent", data);
-  };
+
   return (
     <div>
       <Form {...form}>
